e0e3659e88ef3d19d140bb7af7d9ef6f36d89e5a
James Sample <jamesgsample@gmail.com>
2022-10-21 07:58:38 +0100

    Added hunger and fitness

--- _test_/pet.test.js
+++ _test_/pet.test.js
@@ -8,4 +8,26 @@ describe('constructor', () => {
         const pet = new Pet('Fido');
         expect(pet.name).toEqual('Fido');
     });
+    it('sets the age of the pet', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toBe(0);
+    })
 });
+    describe('growUp', () => {
+        it ('incriments the age by 1', () => {
+        const pet = new Pet ('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    });
+});
+    describe('hungry and unfit with age', () => {
+        it('increases hunger and reduces fitness as the pet ages', () => {
+            const pet = new Pet ('Fido');
+            expect(pet.hunger).toEqual(0);
+            expect(pet.fitness).toEqual(10);
+            pet.growUp();
+            expect(pet.hunger).toBe(5);
+            expect(pet.fitness).toBe(7);
+        });
+    });
+
